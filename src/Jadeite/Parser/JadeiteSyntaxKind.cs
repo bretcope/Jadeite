831273684f94b20fc54a76e602937d57e60ee56a
Bret Copeland <bret@atlantisflight.org>
2015-12-20 21:25:38 -0500

    Move JadeiteSyntaxKind to its own file

---
+++ src/Jadeite/Parser/JadeiteSyntaxKind.cs
@@ -0,0 +1,129 @@
+﻿
+namespace Jadeite.Parser
+{
+    public enum JadeiteSyntaxKind
+    {
+        // ============= TERMINALS =================================================
+
+        [StartOf(SyntaxCategory.InvalidToken)]
+        InvalidToken = 0,
+
+        [StartOf(SyntaxCategory.Whitespace)]
+        EndOfInput,
+        Indent,
+        Outdent,
+        EndOfLine,
+
+        [StartOf(SyntaxCategory.Punctuation)]
+        Hash,
+        Dot,
+        Pipe,
+        PipePipe,
+        PipeEquals,
+        And,
+        AndAnd,
+        AndEquals,
+        Caret,
+        CaretEquals,
+        Plus,
+        PlusPlus,
+        PlusEquals,
+        Minus,
+        MinusMinus,
+        MinusEquals,
+        Asterisk,
+        AsteriskEquals,
+        ForwardSlash,
+        ForwardSlashEquals,
+        Percent,
+        PercentEquals,
+        Equals,
+        EqualsEquals,
+        LessThan,
+        LessThanLessThan,
+        LessThanEquals,
+        LessThanLessThanEquals,
+        GreaterThan,
+        GreaterThanGreaterThan,
+        GreaterThanEquals,
+        GreaterThanGreaterThanEquals,
+        Comma,
+        SemiColon,
+        Bang,
+        BangEquals,
+        QuestionMark,
+        Colon,
+        OpenParen,
+        CloseParen,
+        OpenEscapedInterpolation,    // #{
+        OpenNonEscapedInterpolation, // !{
+        OpenCurly,
+        CloseCurly,
+        OpenTagInterpolation, // #[
+        OpenSquareBracket,
+        CloseSquareBracket,
+
+        // ALL KEYWORD SECTIONS MUST BE CONSECUTIVE
+
+        [StartOf(SyntaxCategory.TemplateKeyword)]
+        AppendKeyword,
+        AttributesKeyword,
+        BlockKeyword,
+        DoctypeKeyword,
+        ExtendsKeyword,
+        IncludeKeyword,
+        MixinKeyword,
+        PrependKeyword,
+
+        [StartOf(SyntaxCategory.CodeKeyword)]
+        BreakKeyword,
+        CaseKeyword,
+        ConstKeyword,
+        ContinueKeyword,
+        DefaultKeyword,
+        ElseKeyword,
+        FalseKeyword,
+        FuncKeyword,
+        IfKeyword,
+        InKeyword,
+        LoopKeyword,
+        ModelKeyword,
+        NullKeyword,
+        ReturnKeyword,
+        SwitchKeyword,
+        TrueKeyword,
+        VarKeyword,
+
+        [StartOf(SyntaxCategory.TypeKeyword)]
+        BoolKeyword,
+        ByteKeyword,
+        CharKeyword,
+        DoubleKeyword,
+        IntKeyword,
+        LongKeyword,
+        SByteKeyword,
+        ShortKeyword,
+        StringKeyword,
+        UIntKeyword,
+        ULongKeyword,
+        UShortKeyword,
+
+        [StartOf(SyntaxCategory.TemplateLiteral)]
+        BufferedHtmlComment,
+        UnbufferedHtmlComment,
+        HtmlIdentifier,
+        HtmlText,
+
+        [StartOf(SyntaxCategory.CodeLiteral)]
+        CodeComment,
+        CodeIdentifier,
+        IntegerLiteral,
+        FloatingPointLiteral,
+        StringLiteral,
+
+        // ============= NON-TERMINALS =============================================
+
+        [StartOf(SyntaxCategory.InvalidNode)]
+        InvalidNode,
+    }
+}
